cc87e4649366747ac665512ee9460ecc0ef59746
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-01-12 14:59:11 +0600

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../pages/shared/Navbar";
+import Footer from "../pages/shared/Footer";
+
+const Main = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <div className="max-w-7xl mx-auto">
+        <Outlet></Outlet>
+        </div>
+      
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
