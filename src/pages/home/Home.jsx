082353d1a4769e7b893c1739a19f1765361a923e
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-01-13 14:18:01 +0600

    new

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,22 @@
+import SectionHeading from "../../Components/SectionHeading";
 import Banner from "./Banner";
+import Category from "./Category";
+import Menu from "./Menu";
+import SevenStar from "./SevenStar";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <SectionHeading title="From 11:00am to 10:00pm"
+      heading="ORDER ONLINE">
+      </SectionHeading>
+      <Category></Category>
+      <SevenStar></SevenStar>
+      <SectionHeading title="Check it out"
+      heading="FROM OUR MENU">
+      </SectionHeading>
+      <Menu></Menu>
     </div>
   );
 };
---
+++ src/pages/home/SevenStar.jsx
@@ -0,0 +1,13 @@
+import "./sevenStar.css";
+const SevenStar = () => {
+  return (
+    <div className="seven-star">
+      <div className="seven-star-1">
+        <h1 className="text-3xl font-bold mb-6">Seven Star</h1>
+        <p className="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore vitae id assume <br />Perferendis, autem. Fuga dicta optio,ipsam blanditiis eius laborum totam  recusandae inventore  <br />dolor ad nisi asperiores ipsum impedit?</p>
+      </div>
+    </div>
+  );
+};
+
+export default SevenStar;
